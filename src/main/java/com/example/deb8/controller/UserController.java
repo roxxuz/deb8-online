2390eb35cf7df3876d64a1019a709fcf0156fcbd
Daniel <d.wallgren1987@gmail.com>
2021-05-11 21:05:56 +0200

    GetMapping /register
    added Getmapping for /register to view signup_form.html

--- src/main/java/com/example/deb8/controller/UserController.java
+++ src/main/java/com/example/deb8/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.deb8.controller;
 
+import com.example.deb8.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +11,11 @@ public class UserController {
     @GetMapping("")
     public String viewFrontPage() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String viewSignUpForm(Model model) {
+        model.addAttribute("user", new User());
+        return "signup_form";
     }
 }
