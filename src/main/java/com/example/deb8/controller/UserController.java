a003c834aefabb1dcf349b3c65425c879f774f8f
Daniel <d.wallgren1987@gmail.com>
2021-05-11 23:54:51 +0200

    Added Postmapping for /process_registration

--- src/main/java/com/example/deb8/controller/UserController.java
+++ src/main/java/com/example/deb8/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.example.deb8.controller;
 
 import com.example.deb8.entity.User;
+import com.example.deb8.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("")
     public String viewFrontPage() {
@@ -18,4 +24,12 @@ public class UserController {
         model.addAttribute("user", new User());
         return "signup_form";
     }
+
+    @PostMapping("/process_registration")
+    public String processRegistration(User user) {
+        userRepository.save(user);
+
+        return "registration_sucess";
+    }
+
 }
