8f03d791bb2d77f2f079a68c5f7770f579fa3a49
Daniel <d.wallgren1987@gmail.com>
2021-05-13 00:32:33 +0200

    Controller mapping for login user

--- src/main/java/com/example/deb8online/controller/UserController.java
+++ src/main/java/com/example/deb8online/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -16,12 +17,10 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private UserRepository userRepository;
-
     //********************** Renderar förstasidan index.html ***********************//
     @GetMapping("/")
-    public String viewIndex(){
+    public String viewIndex(Model model){
+        model.addAttribute("user", new User());
 
         return "index";
     }
@@ -43,6 +42,29 @@ public class UserController {
                 ". You are member nr " + userService.getUserCount());
 
         return "registration_succeded";
+    }
+
+    //************* Kontrollerar användarnamn och lösenord mot databasen *************//
+    @PostMapping("/process_login")
+    public String process_login(User user, Model model){
+
+        if(userService.authUser(user)){
+            return "redirect:/message_board/" + userService.getUserByUserName(user.getUserName()).getId();
+        }
+        else{
+            model.addAttribute("msg", "Incorrect username or password!");
+            return "index";
+        }
+    }
+
+    @GetMapping("/message_board/{id}")
+    public String viewMessageBoard(@PathVariable Long id, Model model){
+
+        User user = userService.getUserById(id);
+
+        model.addAttribute(user);
+
+        return "msg_board";
     }
 
 }
