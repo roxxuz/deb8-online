047e5c836ef40c7d93dfa28fcc6b3f676365d2e5
Daniel <d.wallgren1987@gmail.com>
2021-05-12 17:25:23 +0200

    Added UserRepository interface

---
+++ src/main/java/com/example/deb8online/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.deb8online.repository;
+
+import com.example.deb8online.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
