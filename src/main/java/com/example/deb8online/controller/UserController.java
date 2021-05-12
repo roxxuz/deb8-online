c826f0fcc65ed9aa4fb1f06003469a422a94c4e6
Daniel <d.wallgren1987@gmail.com>
2021-05-12 18:08:49 +0200

    GetMapping for root path

--- src/main/java/com/example/deb8online/controller/UserController.java
+++ src/main/java/com/example/deb8online/controller/UserController.java
@@ -1,13 +1,23 @@
 package com.example.deb8online.controller;
 
+import com.example.deb8online.entity.User;
 import com.example.deb8online.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class UserController {
 
-    @Autowired
-    private UserService userService;
-    
+    //@Autowired
+    //private UserService userService;
+
+    //********************** Renderar förstasidan index.html ***********************//
+    @GetMapping("/")
+    public String welcome(){
+
+        return "index";
+    }
+
 }
