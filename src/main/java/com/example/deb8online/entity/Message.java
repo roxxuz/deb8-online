00fe2d940ce69982de8ffea56291734c73f66eba
Daniel <d.wallgren1987@gmail.com>
2021-05-13 21:49:17 +0200

    Added Message Entity

---
+++ src/main/java/com/example/deb8online/entity/Message.java
@@ -0,0 +1,54 @@
+package com.example.deb8online.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="messages")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime dateTime;
+
+    private String text;
+    private long messageUserId;
+
+    public Message() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public long getMessageUserId() {
+        return messageUserId;
+    }
+
+    public void setMessageUserId(long user_id) {
+        this.messageUserId = user_id;
+    }
+}
