b92cabd773e3bcf2705fa560a60789692293e0b3
Daniel <d.wallgren1987@gmail.com>
2021-05-14 18:42:09 +0200

    Added saveMesssage method in service class

--- src/main/java/com/example/deb8online/service/MessageService.java
+++ src/main/java/com/example/deb8online/service/MessageService.java
@@ -1,5 +1,9 @@
 package com.example.deb8online.service;
 
+import com.example.deb8online.entity.Message;
+import com.example.deb8online.entity.User;
+import com.example.deb8online.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -7,6 +11,14 @@ import java.time.format.DateTimeFormatter;
 
 @Service
 public class MessageService {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public void saveMessage(Message message){
+
+        messageRepository.save(message);
+    }
 
     public LocalDateTime getCurrentDateTime() {
         //DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
