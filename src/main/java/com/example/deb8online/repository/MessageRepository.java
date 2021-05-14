d04d273f78e36f813493bc68492780f488ec3605
Daniel <d.wallgren1987@gmail.com>
2021-05-15 01:21:38 +0200

    All messages from database
    Repository method in Message interface. Find all messages and order them in descending order.

--- src/main/java/com/example/deb8online/repository/MessageRepository.java
+++ src/main/java/com/example/deb8online/repository/MessageRepository.java
@@ -4,8 +4,12 @@ import com.example.deb8online.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
     Message findByMessageUserId(long messageUserId);
+
+    List<Message> findAllByOrderByIdDesc();
 }
