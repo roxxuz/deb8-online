aee6981d6f583de15b3bcecc3c626add9d7f509c
Daniel <d.wallgren1987@gmail.com>
2021-05-14 18:50:47 +0200

    troubleshooting missing field after HTML form

--- src/main/java/com/example/deb8online/controller/MessageController.java
+++ src/main/java/com/example/deb8online/controller/MessageController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -29,10 +30,26 @@ public class MessageController {
 
         Message message = new Message();
 
-        model.addAttribute(user);
-        model.addAttribute(message);
+        message.setMessageUserName(user.getUserName());
 
+        model.addAttribute("user", user);
+        model.addAttribute("message", message);
+
+        System.out.println("--> INNAN HTML FORM - message.userName: " + message.getMessageUserName());
+        System.out.println("--> INNAN HTML FORM - message.text: " + message.getText());
+        System.out.println("=========================================");
         return "msg_board";
+    }
+
+    @PostMapping("/save_message")
+    public String saveMessage(@ModelAttribute Message message, User user){
+
+        System.out.println("--> EFTER HTML FORM - message.userName: " + message.getMessageUserName());
+        System.out.println("--> EFTER HTML FORM - message.text: " + message.getText());
+
+        messageService.saveMessage(message);
+
+        return "redirect:/";
     }
 
 }
