65e0bd7f4d31581e22c61e92a5d9d97547e36eac
Daniel <d.wallgren1987@gmail.com>
2021-05-15 23:03:17 +0200

    deleteMessage() method

--- src/main/java/com/example/deb8online/service/MessageService.java
+++ src/main/java/com/example/deb8online/service/MessageService.java
@@ -21,6 +21,13 @@ public class MessageService {
         message.setDateTime(getCurrentDateTime());
 
         messageRepository.save(message);
+
+        //Hämtar ID efter att det har autogenererats i databsen och tilldelar messageID
+        //Detta var för att formuläret för delete i message_board.html behöver
+        //olika namn på id för User och id för Message.
+        message.setMessageId(message.getId());
+
+        messageRepository.save(message);
     }
 
     public List<Message> getAllMessages(){
@@ -32,5 +39,23 @@ public class MessageService {
         LocalDateTime now = LocalDateTime.now();
 
         return now;
+    }
+
+    public boolean deleteMessage(Message message, User user){
+
+        if(message.getMessageUserId() == user.getId()){
+
+            //Fix för att thymeleaf formuläret i msg_board.html ger message.id samma värde som
+            // user.id av någon anledning. Här sätts rätt message.id igen för
+            // att delete ska fungera med rätt id i databasen.
+            message.setId(message.getMessageId());
+
+            messageRepository.delete(message);
+            System.out.println("-> message id = " + message.getMessageId());
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
