83f01a272e7f62d939875673149543f1911c831f
Daniel <d.wallgren1987@gmail.com>
2021-05-19 19:07:27 +0200

    Service method to update user info

--- src/main/java/com/example/deb8online/service/UserService.java
+++ src/main/java/com/example/deb8online/service/UserService.java
@@ -15,7 +15,7 @@ public class UserService {
 
     public void saveUser(User user){
         //Sätter default profilbild
-        user.setImg("https://via.placeholder.com/150");
+        user.setImg("https://i.pinimg.com/564x/d9/56/9b/d9569bbed4393e2ceb1af7ba64fdf86a.jpg");
 
         userRepository.save(user);
     }
@@ -45,5 +45,15 @@ public class UserService {
         User DBuser = userRepository.findByUserName(userLoginAttempt.getUserName());
 
         return DBuser!= null && DBuser.getPassword().equals(userLoginAttempt.getPassword());
+    }
+
+    public void updateUser(User user){
+        System.out.println("--->" + user.getUserName());
+        //Om inget nytt lösenord angavs så ska nuvarande lösenord behållas.
+        if(user.getPassword().equals("")){
+            user.setPassword(userRepository.findByUserName(user.getUserName()).getPassword());
+        }
+
+        userRepository.save(user);
     }
 }
